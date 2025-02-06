2fe4609cd9abec11984f66998a4fe313ded50418
Elmer Soliz <elmer.soliz@uab.edu.bo>
2025-02-06 01:20:22 +0000

    Primer Commit

---
+++ resume-react/src/component/Presentation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css'
+
+export const Presentation = () => {
+  return (
+    <div class="header-content">
+      <div class="profile-picture">
+        <img src="./perfil.png" alt="Foto de perfil de Elmer Soliz"/>
+      </div>
+      <div class="info">
+        <h1>Elmer Soliz</h1>
+        <p>Desarrollador Web</p>
+        <p>Email: juanperez@email.com | Teléfono: 123-456-7890</p>
+        <p>Dirección: Calle Ficticia 123, Ciudad, País</p>
+      </div>
+    </div>
+ )
+}
